5a1bd9dbb532d156cf070f4dd58f292bf3347c16
Aled Owen <aled.owen@metoffice.gov.uk>
2022-11-07 15:51:10 +0000

    Factor out formatting of total

--- tdd/test_kata_5_barcode.py
+++ tdd/test_kata_5_barcode.py
@@ -11,20 +11,25 @@ BARCODE_DATABASE = {
 }
 
 
+def format_to_currency(total):
+    return "$" + "{:.2f}".format(total)
+
+
 def scan_barcode(input_code):
     """
     """
-
     if input_code == "":
         return "Error: empty barcode"
 
     if input_code == "99999":
         return "Error: barcode not found"
 
-    return "$" + "{:.2f}".format(BARCODE_DATABASE[input_code])
+    value = BARCODE_DATABASE[input_code]
+
+    return format_to_currency(value)
 
 
-test_inputs = {
+end_to_end_test_inputs = {
     "12345": ("12345", "$7.25"),
     "23456": ("23456", "$12.50"),
     "99999": ("99999", "Error: barcode not found"),
@@ -33,9 +38,26 @@ test_inputs = {
 
 
 @pytest.mark.parametrize(
-    "input_code,output", test_inputs.values(), ids=test_inputs.keys()
+    "input_code,output",
+    end_to_end_test_inputs.values(),
+    ids=end_to_end_test_inputs.keys(),
 )
 def test_known_query(input_code, output):
-    """
+    """ Test a known query against the database of values.
     """
     assert scan_barcode(input_code) == output
+
+
+format_test_inputs = {
+    "2dp": (7.25, "$7.25"),
+    "1dp": (12.5, "$12.50"),
+}
+
+
+@pytest.mark.parametrize(
+    "input_number,output", format_test_inputs.values(), ids=format_test_inputs.keys()
+)
+def test_currency_formatting(input_number, output):
+    """ Test that currency values get set properly.
+    """
+    assert format_to_currency(input_number) == output
